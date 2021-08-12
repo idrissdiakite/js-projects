d04af4a9bcbbeaccda77ac73203aeccc3f930215
idrissdiakite <idriss.diakite@hotmail.fr>
2021-08-12 14:02:54 +0200

    :memo: Add Javascript comment

--- 08-tabs/app.js
+++ 08-tabs/app.js
@@ -4,11 +4,14 @@ const contents = document.querySelectorAll(".content");
 
 tabs.addEventListener("click", function (e) {
   const id = e.target.dataset.id;
+  console.log(id)
 
   if (id) {
     btns.forEach(function (btn) {
+      // je retire la classe "active" sur tous les boutons
       btn.classList.remove("active");
     });
+    // j'ajoute la classe "active" uniquement au bouton cliqué
     e.target.classList.add("active");
 
     contents.forEach(function (content) {
